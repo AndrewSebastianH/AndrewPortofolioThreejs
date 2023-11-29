b44cfcffeb7363f3e4076795eee8ff6c3f78df7f
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2023-11-30 00:03:24 +0700

    Animejs setup, added loading spinners, fadein / fadeout animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 'use client'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import anime from 'animejs/lib/anime.es.js'
 
 export default function ThreeComponent() {
+  const [webLoading, setWebLoading] = useState(true)
+  const [loadingAppear, setLoadingAppear] = useState(true)
+
   useEffect(() => {
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(
@@ -14,34 +18,32 @@ export default function ThreeComponent() {
     )
 
     const geometry = new THREE.SphereGeometry(14, 64, 64)
-    const sphereTexture = new THREE.TextureLoader().load('images/sphere1.jpg')
+    const sphereTexture = new THREE.TextureLoader().load(
+      'images/sphere1.jpg',
+      () => {
+        setWebLoading(false)
+      },
+    )
     const sphere = new THREE.Mesh(
       new THREE.SphereGeometry(14, 64, 64),
       new THREE.MeshStandardMaterial({
         color: 0xffffff,
         map: sphereTexture,
-        opacity: 0.5,
-        transparent: true,
       }),
     )
 
     scene.add(sphere)
 
-    // const segmentMaterial = new THREE.MeshStandardMaterial({
-    //   color: 0xffffff,
-    // })
-    // const wireframeMaterial = new THREE.MeshBasicMaterial({
-    //   color: 0x00febb,
-    //   wireframe: true,
-    //   opacity: 0.175,
-    //   transparent: true,
-    // })
+    const wireframeMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      wireframe: true,
+      opacity: 0.075,
+      transparent: true,
+    })
 
-    // const segmentSphere = new THREE.Mesh(geometry, segmentMaterial)
-    // const wireframeSphere = new THREE.Mesh(geometry, wireframeMaterial)
+    const wireframeSphere = new THREE.Mesh(geometry, wireframeMaterial)
 
-    // scene.add(segmentSphere)
-    // scene.add(wireframeSphere)
+    scene.add(wireframeSphere)
 
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#bg'),
@@ -54,12 +56,9 @@ export default function ThreeComponent() {
     camera.position.z = 20
 
     const pointLight = new THREE.PointLight(0xffffff, 40)
-    // const ambientLight = new THREE.AmbientLight(0x404040, 10)
     pointLight.position.set(4, 7, 21)
     scene.add(pointLight)
-    // scene.add(ambientLight)
 
-    // Helpers
     const lightHelper = new THREE.PointLightHelper(pointLight)
     const gridHelper = new THREE.GridHelper(200, 50)
     scene.add(lightHelper, gridHelper)
@@ -81,12 +80,10 @@ export default function ThreeComponent() {
     const animate = () => {
       requestAnimationFrame(animate)
 
-      sphere.rotation.x += 0.0025
-      sphere.rotation.y += 0.0025
-      // segmentSphere.rotation.x += 0.0025
-      // segmentSphere.rotation.y += 0.0025
-      // wireframeSphere.rotation.x += 0.0025
-      // wireframeSphere.rotation.y += 0.0025
+      sphere.rotation.x += 0.0015
+      sphere.rotation.y += 0.0015
+      wireframeSphere.rotation.x += 0.0015
+      wireframeSphere.rotation.y += 0.0015
 
       orbitControl.update()
 
@@ -95,28 +92,70 @@ export default function ThreeComponent() {
 
     animate()
 
-    // Cleanup function
     return () => {
       geometry.dispose()
       sphereTexture.dispose()
-      // segmentMaterial.dispose()
-      // wireframeMaterial.dispose()
       renderer.dispose()
     }
   }, [])
 
+  useEffect(() => {
+    anime({
+      targets: '.loader-container',
+      keyframes: [{ opacity: 1 }, { opacity: 0 }],
+      duration: 2000,
+      loop: false,
+      easing: 'easeOutQuad',
+    })
+
+    setTimeout(() => {
+      setLoadingAppear(false)
+    }, 2500)
+  }, [!webLoading])
+
+  useEffect(() => {
+    anime({
+      targets: '.land-page',
+      keyframes: [{ opacity: 0 }, { opacity: 1 }],
+      duration: 1000,
+      loop: false,
+      easing: 'easeOutQuad',
+    })
+  }, [!loadingAppear])
+
   return (
     <>
       <canvas id="bg" className="fixed top-0 left-0" />
-      <div className="absolute w-full h-full">
-        <div className="font-aqua text-lg gap-5 flex flex-row justify-end m-2.5 text-white">
-          <div>Profile</div>
-          <div>Projects</div>
+      {loadingAppear && (
+        <div
+          className="loader-container absolute bg-neutral-800 w-full h-full flex justify-center items-center"
+          style={{
+            opacity: 1,
+          }}
+        >
+          <div className="loader" />
         </div>
-        <div className="font-aqua flex justify-center -mt-10 items-center h-full ] text-white text-3xl tracking-wider">
-          ANDREW SEBASTIAN HARDIANTA
+      )}
+      {!loadingAppear && (
+        <div
+          className="land-page absolute w-full h-full"
+          style={{
+            opacity: 0,
+          }}
+        >
+          <div className="font-aqua text-lg gap-5 flex flex-row justify-end m-2.5 text-white">
+            <div>
+              <a href="#">Profile</a>
+            </div>
+            <div>
+              <a href="#">Work</a>
+            </div>
+          </div>
+          <div className="font-aqua flex justify-center -mt-10 items-center h-full name text-white text-3xl tracking-wider">
+            ANDREW SEBASTIAN HARDIANTA
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
